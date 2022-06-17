517c10d147f1e50b2d9a37259f6439a7bbd4a818
Matt Smrke <smrkem@gmail.com>
2022-06-17 17:12:13 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <header className="">
+        <h1 className='container'>
+          SLS REACT SCAFFOLD
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
